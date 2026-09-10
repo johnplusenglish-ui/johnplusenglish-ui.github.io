67c2d9e5b0ea4fd017cc04eacabba66b0e01e10a
John O'Brien <johnplusenglish@gmail.com>
2026-09-10 11:58:25 +0200

    Make PISA stats page public and add johnplusenglish branding

--- lock.js
+++ lock.js
@@ -1,10 +1,12 @@
 (function(){
   var KEY = 'jpe-unlocked-v1';
+  var PUBLIC = ['pisa-stats.html'];
+  var here = location.pathname.split('/').pop() || 'index.html';
+  if (here === 'lock.html') return;
+  if (PUBLIC.indexOf(here) > -1) return;
   try {
     if (sessionStorage.getItem(KEY) === '1') return;
   } catch(e) {}
-  var here = location.pathname.split('/').pop() || 'index.html';
-  if (here === 'lock.html') return;
   var ret = encodeURIComponent(here + location.search);
   location.replace('lock.html?r=' + ret);
 })();
